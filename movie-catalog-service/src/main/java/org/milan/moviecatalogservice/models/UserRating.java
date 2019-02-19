c8756a8055eeebf0a1212646af621550635b22b5
mirathod <milan.rathod0510@gmail.com>
2019-02-19 17:22:56 +0530

    added service discovery feature

---
+++ movie-catalog-service/src/main/java/org/milan/moviecatalogservice/models/UserRating.java
@@ -0,0 +1,21 @@
+package org.milan.moviecatalogservice.models;
+
+import java.util.List;
+
+/**
+ * Pojo for User Rating
+ *
+ * @author Milan Rathod
+ */
+public class UserRating {
+
+    private List<Rating> ratings;
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+}
--- movie-catalog-service/src/main/java/org/milan/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/org/milan/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,14 +2,13 @@ package org.milan.moviecatalogservice.resources;
 
 import org.milan.moviecatalogservice.models.CatalogItem;
 import org.milan.moviecatalogservice.models.MovieInfo;
-import org.milan.moviecatalogservice.models.Rating;
+import org.milan.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,13 +27,10 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5679", 3)
-        );
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratings/users/" + userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
-            MovieInfo movieInfo = restTemplate.getForObject("http://localhost:5002/movies/" + rating.getMovieId(), MovieInfo.class);
+        return ratings.getRatings().stream().map(rating -> {
+            MovieInfo movieInfo = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), MovieInfo.class);
             return new CatalogItem(movieInfo.getName(), movieInfo.getDesc(), rating.getRating());
         }).collect(Collectors.toList());
     }
